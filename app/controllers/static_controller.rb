02b47b79432bb08ff711237222e06b88f06d8e0a
Khanh Nguyen <khanhnn@nustechnology.com>
2017-03-04 15:54:36 +0700

    TDD for static#homepage

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,7 @@
+class StaticController < ApplicationController
+
+  def homepage
+
+  end
+
+end
---
+++ spec/features/static_spec.rb
@@ -0,0 +1,12 @@
+# spec/features/static_spec.rb
+
+require 'rails_helper'
+
+describe 'navigate' do
+  describe 'homepage' do
+    it 'can be reached successfully' do
+      visit root_path
+      expect(page.status_code).to eq(200)
+    end
+  end
+end
