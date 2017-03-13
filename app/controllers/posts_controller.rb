a92289e6d275f8cb778a0038595bffb752c38030
Khanh Nguyen <khanhhq2k@gmail.com>
2017-03-13 17:49:35 +0700

    Develop Post creation by BDD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit([:date, :rationale]))
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 end
