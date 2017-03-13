30a21478696b3828b1a8812baf6f28f2491a7ea1
Khanh Nguyen <khanhhq2k@gmail.com>
2017-03-13 19:18:35 +0700

    Add a little hack to bypass belongs_to :user, optional: true(default)

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,19 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-	describe 'creation' do
-		before do
-			@post = Post.create(date: Date.today, rationale: "Anythingg!!")
-		end
-		it 'can be created' do
-			expect(@post).to be_valid
-		end
+  describe 'creation' do
+    before do
+      # a little hack to bypass belongs_to :user, optional: true(default)
+      user = User.create(email: "test@abc.com", password: "asdasd", password_confirmation: "asdasd", first_name: "John", last_name: "Snow")
 
-		it 'cannot be created without date and rationale' do
-			@post.date = nil
-			@post.rationale = nil
-			expect(@post).to_not be_valid
-		end
-	end
+      @post = Post.create(date: Date.today, rationale: "Anythingg!!", user_id: user.id)
+    end
+    it 'can be created' do
+      expect(@post).to be_valid
+    end
+
+    it 'cannot be created without date and rationale' do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).to_not be_valid
+    end
+  end
   
 end
