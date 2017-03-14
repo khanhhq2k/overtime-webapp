14de14bee99747a9c3c82cc5bcd8480f283305d4
Khanh Nguyen <khanhhq2k@gmail.com>
2017-03-14 12:52:14 +0700

    Render Posts index via BDD

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
+user = User.create(email: "test@abc.com", password: "asdasd", password_confirmation: "asdasd", first_name: "John", last_name: "Snow")
+puts "1 user created!"
+
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "rationale content #{post}")
+	Post.create!(date: Date.today, rationale: "rationale content #{post}", user_id: user.id)
 end
 
 puts "100 posts have been created!"
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,22 +1,34 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    @user = User.create(email: "test@abc.com", password: "asdasd", password_confirmation: "asdasd", first_name: "John", last_name: "Snow")
+    login_as(@user, :scope => :user)
+  end
+
   describe 'index' do
-    it 'can be reached' do
+    before do
       visit posts_path
+    end
+    it 'can be reached' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "post1!", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "post2!", user_id: @user.id)
+
+      visit posts_path
+      expect(page).to have_content(/post1!|post2!/)
     end
   end
 
   describe 'creation' do
     before do
-      user = User.create(email: "test@abc.com", password: "asdasd", password_confirmation: "asdasd", first_name: "John", last_name: "Snow")
-      login_as(user, :scope => :user)
       visit new_post_path
     end
 
