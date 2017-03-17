ed2c1e153382cc6451bc66061d1b51be3c70c944
Khanh Nguyen <khanhhq2k@gmail.com>
2017-03-17 18:18:08 +0700

    test visibility of non-admin user to Post status(Must be unable to see)

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -10,13 +10,23 @@ describe 'navigate' do
     before do
       @post = FactoryGirl.create(:post)
     end
-    it 'has a status that can be edited in the form' do
+
+    it 'has a status that can be edited by an Admin User' do
       visit edit_post_path(@post)
 
       choose 'post_status_approved'
       click_on "Update"
 
       expect(@post.reload.status).to eq('approved')
+    end
+
+    it 'cannot be edited by an NON Admin User' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, scope: :user)
+      visit edit_post_path(@post)
+
+      expect(page).to_not have_content('Approved')
     end
   end
 end
