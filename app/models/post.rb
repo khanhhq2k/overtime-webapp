4f261a0ad3bd2a5e6656ffa25389eb517d05a869
Khanh Nguyen <khanhhq2k@gmail.com>
2017-03-17 15:12:19 +0700

    Add status as enum for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
 	belongs_to :user
 	validates_presence_of :date, :rationale
-
+  enum status: { submitted: 0, approved: 1, rejected: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313111027) do
+ActiveRecord::Schema.define(version: 20170317074330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,10 @@ ActiveRecord::Schema.define(version: 20170313111027) do
   create_table "posts", force: :cascade do |t|
     t.date     "date"
     t.text     "rationale"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.integer  "user_id"
+    t.integer  "status",     default: 0
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
