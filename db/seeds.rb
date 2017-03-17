19a27d7cad605686324da4762ccd6b2e79599747
Khanh Nguyen <khanhhq2k@gmail.com>
2017-03-17 17:42:22 +0700

    Add radio button for Post status and amend seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 user = User.create(email: "test@abc.com", password: "asdasd", password_confirmation: "asdasd", first_name: "John", last_name: "Snow")
 puts "1 user created!"
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name")
+puts "1 Admin User created"
 
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "rationale content #{post}", user_id: user.id)
