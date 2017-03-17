8789ec077b21bf96b637e5ba2abefb181e11e11b
Khanh Nguyen <khanhhq2k@gmail.com>
2017-03-18 01:32:41 +0700

    Redirect for unauthorized action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to access this resource"
+    redirect_to(request.referrer || root_path)
+  end
 end
