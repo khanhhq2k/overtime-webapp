01de33b7d1568ebcb3a09dcf67bfb69a1b9c26d2
Khanh Nguyen <khanhhq2k@gmail.com>
2017-03-19 01:09:14 +0700

    add Post index scope for admin and normal users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,11 @@ class PostsController < ApplicationController
 
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
-    @posts = Post.all
+    @posts = if ADMIN_USER_TYPES.include?(current_user.type)
+      Post.all
+    else
+      Post.user_posts(current_user)
+    end
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
 	belongs_to :user
 	validates_presence_of :date, :rationale
   enum status: { submitted: 0, approved: 1, rejected: 2 }
+  scope :user_posts, ->(user) {where(user_id: user.id)}
 end
