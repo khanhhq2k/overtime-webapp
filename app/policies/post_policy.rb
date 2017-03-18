0667fcfae011bb8875a40618ca23076ec5384f7f
Khanh Nguyen <khanhhq2k@gmail.com>
2017-03-19 01:42:42 +0700

    Update authorization rule for edit record, add test for approval workflow via BDD

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,19 @@
 class PostPolicy < ApplicationPolicy
   def update?
-    record.user_id == user.id || ADMIN_USER_TYPES.include?(user.type)
+    #logic for update:
+    #admin can update any records, despite record's status
+    #users can only edit their records and the records status must not be approved yet
+    is_admin? || (is_owner? && !post_approved?)
+  end
+
+  private
+  def is_admin?
+    ADMIN_USER_TYPES.include?(user.type)
+  end
+  def is_owner?
+    record.user_id == user.id
+  end
+  def post_approved?
+    record.approved?
   end
 end
