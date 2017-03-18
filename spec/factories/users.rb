1f1c746b67e38aea5c2ae3d6afe2b70bb4e6d3f7
Khanh Nguyen <khanhhq2k@gmail.com>
2017-03-18 13:32:33 +0700

    Add Factory Girl method to create a non authorized user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,18 +4,25 @@ FactoryGirl.define do
   end
   factory :user do
     email {generate :email}
-    password "asdasd"
-    password_confirmation "asdasd"
-    first_name "John"
-    last_name "Snow"
+    password 'asdasd'
+    password_confirmation 'asdasd'
+    first_name 'John'
+    last_name 'Snow'
   end
 
-  factory :admin_user, class: "AdminUser" do
+  factory :admin_user, class: 'AdminUser' do
     email {generate :email}
-    password "asdasd"
-    password_confirmation "asdasd"
-    first_name "Admin"
-    last_name "User"
+    password 'asdasd'
+    password_confirmation 'asdasd'
+    first_name 'Admin'
+    last_name 'User'
   end
 
+  factory :non_authorized_user, class: 'User' do
+    email {generate :email}
+    password 'asdasd'
+    password_confirmation 'asdasd'
+    first_name 'Non Authorized Person'
+    last_name 'Snow'
+  end
 end
